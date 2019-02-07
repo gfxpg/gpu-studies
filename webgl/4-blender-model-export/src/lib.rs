888908af351ad0d36ca258a49ee2ec6968e70d1c
timlathy <tim.labushev@gmail.com>
2019-02-07 11:12:06 +0300

    [webgl] .obj does not support vertex colors, switch to .ply

--- webgl/4-blender-model-export/src/lib.rs
+++ webgl/4-blender-model-export/src/lib.rs
@@ -4,12 +4,13 @@ mod interop;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
-use wavefront_obj::obj;
+use ply_rs as ply;
 
 use web_sys::{HtmlCanvasElement, WebGl2RenderingContext};
 
-const FRAGMENT_SHADER_SRC: &'static str = include_str!("../shaders/fragment.glsl");
+const MODEL_PLY: &'static str = include_str!("../cube.ply");
 const VERTEX_SHADER_SRC: &'static str = include_str!("../shaders/vertex.glsl");
+const FRAGMENT_SHADER_SRC: &'static str = include_str!("../shaders/fragment.glsl");
 
 #[wasm_bindgen]
 pub fn load_gl(canvas_js: JsValue) -> Result<(), JsValue> {
@@ -25,14 +26,8 @@ pub fn load_gl(canvas_js: JsValue) -> Result<(), JsValue> {
 
     ctx.use_program(Some(&program));
 
-    let vertices: [f32; 9] = [-0.7, -0.7, 0.0,
-                              0.7, -0.7, 0.0,
-                              0.0, 0.7, 0.0];
+    let (vertices, colors) = load_model().map_err(|e| format!("{}", e))?;
     let vertices_array = interop::get_float32array(&vertices)?;
-
-    let colors: [u8; 9] = [236, 188, 124,
-                           124, 236, 132,
-                           188, 124, 236];
     let colors_array = interop::get_uint8array(&colors)?;
 
     gl::load_attrib(&ctx, &program, "a_position", &vertices_array, 3, WebGl2RenderingContext::FLOAT, false)?;
@@ -46,13 +41,36 @@ pub fn load_gl(canvas_js: JsValue) -> Result<(), JsValue> {
     Ok(())
 }
 
-#[wasm_bindgen]
-pub fn load_vertices(obj_src: String, object_idx: usize) -> Vec<f32> {
-    let obj_set = obj::parse(obj_src).unwrap();
-    let obj = &obj_set.objects[object_idx];
+pub fn load_model() -> std::io::Result<(Vec<f32>, Vec<u8>)> {
+    use ply_rs::ply::PropertyAccess;
 
-    obj.vertices
-        .iter()
-        .flat_map(|&obj::Vertex { x, y, z }| vec![x as f32, y as f32, z as f32])
-        .collect()
+    let parser = ply::parser::Parser::<ply::ply::DefaultElement>::new();
+    let mut model_source = std::io::Cursor::new(MODEL_PLY);
+    let model = parser.read_ply(&mut model_source)?.payload;
+
+    let x_key = "x".to_owned();
+    let y_key = "y".to_owned();
+    let z_key = "z".to_owned();
+
+    let r_key = "red".to_owned();
+    let g_key = "green".to_owned();
+    let b_key = "blue".to_owned();
+
+    let vertices: Vec<f32> = model["vertex"].iter().flat_map(|m| {
+        vec![
+            m.get_float(&x_key).unwrap(),
+            m.get_float(&y_key).unwrap(),
+            m.get_float(&z_key).unwrap()
+        ]
+    }).collect();
+
+    let colors: Vec<u8> = model["vertex"].iter().flat_map(|m| {
+        vec![
+            m.get_uchar(&r_key).unwrap(),
+            m.get_uchar(&g_key).unwrap(),
+            m.get_uchar(&b_key).unwrap()
+        ]
+    }).collect();
+
+    Ok((vertices, colors))
 }
